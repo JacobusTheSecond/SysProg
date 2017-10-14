42b4f9a3b1d7dd56061b6313101ff09feb8a4922
Matthias Schweighöfer <jacobus.conradi@gmail.com>
2017-10-14 13:13:58 +0000

    relocated strcmp

--- Übungsblatt-1/Aufgabe-1/dll.c
+++ Übungsblatt-1/Aufgabe-1/dll.c
@@ -4,20 +4,7 @@
 #include "personen_liste.h"
 
 //All rights reserver to Linus Torvald xDDD
-int strcmp(const char *cs, const char *ct)
-{
-	unsigned char c1, c2;
 
-	while (1) {
-		c1 = *cs++;
-		c2 = *ct++;
-		if (c1 != c2)
-			return c1 < c2 ? -1 : 1;
-		if (!c1)
-			break;
-	}
-	return 0;
-}
 void freeNode(node* destroyer){
 	free(destroyer->vorname);
 	free(destroyer->nachname);
